368011cc495247af5efba53bca24f822f65f9024
Yaşar İsgəndərov <yasharisgenderov.0@gmail.com>
2021-01-22 00:48:39 +0400

    22 Yanvar

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            product.ProductName = "Kamera";
+        }
+
+       // public void BirSeyYap(int eded)
+        //{
+          // eded = 99;
+        //}
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.ID = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product() { ID = 2, CategoryId = 5, UnitsInStock = 5, ProductName = "Kalem", UnitPrice = 35 };
+
+            ProductManager productManager = new ProductManager();
+
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+           // int eded = 100;
+            //productManager.BirSeyYap(eded);
+            //Console.WriteLine(eded);
+          
+            // int,double,bool .......... deger tip
+            // class,arrays,abstract class,interface........referans tip
+        }
+    }
+}
