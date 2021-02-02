d83bc03d3971d55a9bdedb1e18cd3993531545b5
Yaşar İsgəndərov <yasharisgenderov.0@gmail.com>
2021-02-02 14:46:39 +0400

    2 Yanvar 2:43 PM

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -7,7 +7,7 @@ namespace GenericsIntro
         static void Main(string[] args)
         {
             MyList<string> isimler = new MyList<string>();
-            isimler.Add();
+           
         }
     }
 }
